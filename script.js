8f136f697151d31e64f513893c8cd68d007e59b1
Ajit Kumar <ajit62768@gmail.com>
2026-06-09 22:22:03 +0530

    intial commit

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
 const apiKey = "5a0d1bcbdee3437899295538250510";
 
 const searchBtn = document.getElementById("searchBtn");
+const weatherCard = document.querySelector(".weather-card");
 
 async function getWeather(city) {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`;
 
-    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`;
+  try {
+    const response = await fetch(url);
 
-    try {
+    const data = await response.json();
 
-        const response = await fetch(url);
+    if (data.error) {
+      weatherCard.classList.add("hidden");
 
-        const data = await response.json();
+      alert(data.error.message);
 
-        document.getElementById("temp").innerHTML =
-            data.current.temp_c + "°C";
-
-        document.getElementById("cityName").innerHTML =
-            data.location.name;
-        document.getElementById("condition").innerHTML =
-            data.current.condition.text;
-
-        document.getElementById("humidity").innerHTML =
-            data.current.humidity + "%";
-
-        document.getElementById("wind").innerHTML =
-            data.current.wind_kph + " km/h";
-
-        document.getElementById("cloud").innerHTML =
-            data.current.cloud + "%";
-
-        document.getElementById("weatherIcon").src =
-            "https:" + data.current.condition.icon;
-
+      return;
     }
 
-    catch(error){
+    document.getElementById("temp").innerHTML = data.current.temp_c + "°C";
 
-        alert("City Not Found");
+    document.getElementById("cityName").innerHTML = data.location.name;
+    document.getElementById("condition").innerHTML =
+      data.current.condition.text;
 
-    }
+    document.getElementById("humidity").innerHTML = data.current.humidity + "%";
 
+    document.getElementById("wind").innerHTML = data.current.wind_kph + " km/h";
+
+    document.getElementById("cloud").innerHTML = data.current.cloud + "%";
+
+    document.getElementById("weatherIcon").src =
+      "https:" + data.current.condition.icon;
+
+    weatherCard.classList.remove("hidden");
+
+  } catch (error) {
+
+    weatherCard.classList.add("hidden");
+
+    alert("City Not Found");
+  }
 }
 
-searchBtn.addEventListener("click",()=>{
+searchBtn.addEventListener("click", () => {
+  const city = document.getElementById("city").value;
 
-    const city=document.getElementById("city").value;
-
-    getWeather(city);
-
+  getWeather(city);
 });
-
-getWeather("Delhi");
